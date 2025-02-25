d49a673aeb8ef2ff860d6755a5f52eaa36eb2911
HERMINET <herminet@MacBook-Pro-de-HERMINET.local>
2025-02-25 23:44:43 +0100

    mouvement perso

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class Player : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float speed;
+    public float jumpingPower;
+    public LayerMask groundLayer;
+    public Transform groundCheck;
+    float horizontal;
+    SpriteRenderer sr;
+
+    private void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(horizontal * speed, rb.linearVelocity.y);
+    }
+
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        horizontal = context.ReadValue<Vector2>().x;
+        if (horizontal < 0)
+            sr.flipX = true;
+        else
+            sr.flipX = false;
+
+    }
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (context.performed && isGrounded())
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpingPower);
+        }
+    }
+    bool isGrounded()
+    {
+        return Physics2D.OverlapCapsule(groundCheck.position, new Vector2(1f, .1f), CapsuleDirection2D.Horizontal, 0, groundLayer);
+    }
+}
