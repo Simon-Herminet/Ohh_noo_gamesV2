c05c9dec28b47c1cab9c8b4110285a35641042df
HERMINET <herminet@macbook-pro-de-herminet.home>
2025-02-26 17:02:05 +0100

    tmor

--- Assets/Scripts/PlayerColision.cs
+++ Assets/Scripts/PlayerColision.cs
@@ -1,8 +1,11 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.UI;
 public class PlayerColision : MonoBehaviour
 {
     public int life = 3;
+    public TMP_Text GameOverText;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -11,19 +14,26 @@ public class PlayerColision : MonoBehaviour
             TakeDamages(3);
         }
     }
+
     public void TakeDamages(int damage)
     {
         life -= damage;
 
-        Die();
+        if (life <= 0)
+        {
+            Die();
+        }
     }
 
     public void Die()
     {
+        Debug.Log("Le joueur est mort !");
         GetComponent<Rigidbody2D>().AddForce(Vector3.up * 180);
         GetComponent<Collider2D>().isTrigger = true;
-        Invoke("RestartLevel", 1);
+        GameOverText.gameObject.SetActive(true);
+        Invoke("RestartLevel", 5);
     }
+
     public void RestartLevel()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
